94f4031324041bf5a0f11a531e4dde914a68a5c9
gerardnico <gerardnico@gmail.com>
2020-04-20 15:04:40 +0200

    Backup - first version of the story generator

--- src/lib/story.js
+++ src/lib/story.js
@@ -1,3 +1,4 @@
+import * as pathapi from "path";
 
 /**
  * Take a path from a stories and return a path
@@ -6,12 +7,38 @@
  * Path argument would come from
  * https://www.npmjs.com/package/paths.macro
  * 
- * @param {String} base
- * @param {String} filename
+ * @param {String} dirName - the relative directory path with a ending slash
+ * @param {String} fileName - the file name of the stories (optional) - if called from a story
  */
-export function toStoryHierarchy(base, filename) {
-    base = base.substring("/stories/".length,base.length);
-    filename = filename.substr(0, filename.indexOf("."));
-    return (base + filename).replace("_"," ");
+export function toStoryHierarchy(dirName, fileName) {
+
+    dirName = dirName.substring("/stories/".length, dirName.length);
+    if (typeof fileName === 'undefined') {
+        return dirName
+        .replace("_", " ") // replace the _
+        .substr(0,dirName.length-1); // suppress the last /
+    } else {
+        fileName = fileName.substr(0, fileName.indexOf("."));
+        return (dirName + fileName).replace("_", " ");
+    }
+
+}
+
+
+/**
+ * Return a javascript storybook stories based on a
+ * traversal of the gallery
+ * @param dir - a directory where the images are stored
+ */
+export function generate(dir) {
+    let title = toStoryHierarchy(dir)
+    let script =
+        `import { base, filename } from 'paths.macro';
+
+export default {
+    title: "${title}"
+};`
+    return script;
+
 }
 
--- src/lib/story.test.js
+++ src/lib/story.test.js
@@ -1,12 +1,32 @@
 import { base, filename } from 'paths.macro';
-import { toStoryHierarchy } from './story';
+import { toStoryHierarchy, generate } from './story';
 
-test('Example of usage with the paths macro import', () => {
+test('toStoryHiearchy: Example of usage with the paths macro import', () => {
     // stories is added because this is the base
     expect(toStoryHierarchy("/stories"+base, filename)).toBe('src/lib/story');
 });
 
-test('path', () => {
+test('toStoryHiearchy: example with a literal path (given)', () => {
     // manual real path
-    expect(toStoryHierarchy('/stories/sales/','icons.stories.js')).toBe('sales/icons');
+    expect(toStoryHierarchy('/stories/sales/', 'icons.stories.js')).toBe('sales/icons');
+});
+
+test('toStoryHiearchy: example with only a directory path', () => {
+    // manual real path
+    expect(toStoryHierarchy('/stories/sales_process/')).toBe('sales process');
+});
+
+test('generate: example with a literal path (given)', () => {
+    
+    let groupTitle = "process business"
+    const dir = "/stories/"+groupTitle.replace(" ","_")+"/";
+    let script = generate(dir)
+    let expected = 
+`import { base, filename } from 'paths.macro';
+
+export default {
+    title: "${groupTitle}"
+};`
+    expect(script).toBe(expected);
+
 });
